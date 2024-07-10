e290c29c48c0c5994d874eaf1212a89476c9ea96
sabinebarrios <sabibarrios868@gmail.com>
2024-07-10 11:08:19 -0600

    Add files via upload

---
+++ segundoprograma.codeblocks.Sabine.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+    cout<<"OPERACIONES BASICAS"<<endl;
+    cout<<"entradas y salidas"<<endl;
+    cout<<"Ingrese el primer valor"<<endl;
+    cin>>Valor1;
+    cout<<"Ingrese el segundo valor"<<endl;
+    cin>>Valor2;
+    respuesta = valor1 + valor2;
+    cout<<respuesta;
+    return 0;
+    }
